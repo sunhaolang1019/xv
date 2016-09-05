2bf7e0c57bd6013d473a172d37fbff9c9dbc728c
Sunhaolang <sunhaolang1019@sina.com>
2016-09-05 17:29:29 +0800

    asdsadasd

--- 1.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-// 创建一个服务器
-var httpObj = http.createServe(function(request,response){
-    console.log("有人来了");
-    response.write('给你玩牛肉');
-    response.end();
-         
-})
-http.listen(8080);
